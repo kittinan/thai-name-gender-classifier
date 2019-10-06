2ffc15c21b7832c9ffb73c32098d885854368845
Kittinan <tunxedge@gmail.com>
2019-10-06 21:13:16 +0700

    Updates

---
+++ precache-manifest.934ed5f1259b3631d73d53ce36565a18.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d96c4b0d489db0c2554a3b5a06baeb5f",
+    "url": "/thai-name-gender-classifier/index.html"
+  },
+  {
+    "revision": "ab4983641217a77c838f",
+    "url": "/thai-name-gender-classifier/static/css/main.a6cc22b3.chunk.css"
+  },
+  {
+    "revision": "64d684912f32e2b815ae",
+    "url": "/thai-name-gender-classifier/static/js/2.882ce42c.chunk.js"
+  },
+  {
+    "revision": "ab4983641217a77c838f",
+    "url": "/thai-name-gender-classifier/static/js/main.f0b84c31.chunk.js"
+  },
+  {
+    "revision": "da7f28f7f6b439d50078",
+    "url": "/thai-name-gender-classifier/static/js/runtime-main.09f1fc1e.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/thai-name-gender-classifier/static/media/logo.25bf045c.svg"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/thai-name-gender-classifier/precache-manifest.934ed5f1259b3631d73d53ce36565a18.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/thai-name-gender-classifier/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
+});
